e80687b61cafe0ca4f76c55858be6f9c88b101c2
gleporoni <leporoni.1944533@studenti.uniroma1.it>
2023-04-13 14:44:10 +0200

    .

--- src/model/depthfakerocket.py
+++ src/model/depthfakerocket.py
@@ -23,231 +23,139 @@ class DepthFakeRocket(pl.LightningModule):
         self.conf = conf
         self.num_classes = self.conf.data.num_classes
         in_features = 1
-        if self.conf.data.use_hha:
+        if self.conf.data.depth_type  == 'hha':
             in_features = 3
-
 
         if self.conf.model.backbone == "xception":
             self.model = timm.create_model(
                 "xception", pretrained=True, num_classes=self.num_classes
             )
 
-            #load pretrained weights
-            if self.conf.run.double_depth_network.use_pretain:
-                
-                self.rgb_model = RGB(conf)
-                # self.depth_model = RGB(conf)
-                
-                # base_path = Path(Path(__file__).parent, "../../")
-                # rgb_checkpoint = Path(
-                #     base_path,
-                #     self.conf.run.double_depth_network.rgb_checkpoint,
-                # )
-                # depth_checkpoint = Path(
-                #     base_path,
-                #     self.conf.run.double_depth_network.depth_checkpoint,
-                # )
-                
-                # #load weights for rgb part
-                # self.rgb_model = self.rgb_model.load_from_checkpoint(checkpoint_path=rgb_checkpoint)
+            # self.rgb_model = RGB(conf)
+            tmp_model = list(self.model.children())
+
+            # net before block 1
+            self.rgb_model_init = copy.deepcopy(torch.nn.Sequential(*(tmp_model[:6])))
+            self.depth_model_init = copy.deepcopy(torch.nn.Sequential(*(tmp_model[:6])))
+
+            if in_features == 1:
+            #remove rgb input and take only depth input in the first layer
+                weights = self.depth_model_init[0].weight
+                kernel_size = tuple(self.depth_model_init[0].kernel_size)
+                stride = tuple(self.depth_model_init[0].stride)
+                out_features = weights.shape[0]
+                new_features = torch.nn.Conv2d(
+                    in_features, out_features, kernel_size=kernel_size, stride=stride
+                )
+                torch.nn.init.xavier_uniform_(new_features.weight)
+                self.depth_model_init[0] = new_features
 
 
-                # self.depth_model = self.depth_model.load_from_checkpoint(checkpoint_path=depth_checkpoint)
+            # block 1
+            tmp = list(tmp_model[6].children())
+            self.rgb_skip_block_1 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.rgb_conv_block_1 = copy.deepcopy(tmp[2])
 
-                # #remove rgb input and take only depth input in the first layer
-                # # weights = self.depth_model.model.conv1.weight
-                # # kernel_size = tuple(self.depth_model.model.conv1.kernel_size)
-                # # stride = tuple(self.depth_model.model.conv1.stride)
-                # # out_features = weights.shape[0]
-                # # new_features = torch.nn.Conv2d(
-                # #     in_features, out_features, kernel_size=kernel_size, stride=stride
-                # # )
-                # # new_features.weight = torch.nn.Parameter(weights.data[:, 3:, :, :])
-                # # self.depth_model.model.conv1 = new_features
+            self.depth_skip_block_1 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.depth_conv_block_1 = copy.deepcopy(tmp[2])
 
-                # #freeze the models
-                # for param in self.rgb_model.model.parameters():
-                #     param.requires_grad = False
-                # for param in self.depth_model.model.parameters():
-                #     param.requires_grad = False
-                
-                # split = self.conf.run.double_depth_network.split
-                # self.rgb_model = torch.nn.Sequential(*(list(self.rgb_model.model.children())[:split]))
-                # self.depth_model = torch.nn.Sequential(*(list(self.depth_model.model.children())[:split]))
-                # self.concat_layer = torch.nn.Conv2d(
-                #     1456, 728, kernel_size=(1,1), stride=(1,1), padding = (0,0)
-                # )
-                # torch.nn.init.xavier_uniform_(self.concat_layer.weight)
-                # self.model = torch.nn.Sequential(*(list(self.model.children())[split:])) 
-                
-            else:
-                # self.rgb_model = RGB(conf)
-                tmp_model = list(self.model.children())
+            self.concat_block_1 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.concat_block_1[0] = self._fix_concat_layer(self.concat_block_1)
+                            
+            # block 2
+            tmp = list(tmp_model[7].children())
+            self.rgb_skip_block_2 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.rgb_conv_block_2 = copy.deepcopy(tmp[2])
 
-                # net before block 1
-                self.rgb_model_init = copy.deepcopy(torch.nn.Sequential(*(tmp_model[:6])))
-                self.depth_model_init = copy.deepcopy(torch.nn.Sequential(*(tmp_model[:6])))
+            self.depth_skip_block_2 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.depth_conv_block_2 = copy.deepcopy(tmp[2])
 
-                #remove rgb input and take only depth input in the first layer
-                # weights = self.depth_model_init[0].weight
-                # kernel_size = tuple(self.depth_model_init[0].kernel_size)
-                # stride = tuple(self.depth_model_init[0].stride)
-                # out_features = weights.shape[0]
-                # new_features = torch.nn.Conv2d(
-                #     in_features, out_features, kernel_size=kernel_size, stride=stride
-                # )
-                # # new_features.weight = torch.nn.Parameter(weights.data[:, 3:, :, :])
-                # torch.nn.init.xavier_uniform_(new_features.weight)
-                # self.depth_model_init[0] = new_features
+            self.concat_block_2 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.concat_block_2[0] = self._fix_concat_layer(self.concat_block_2)
 
+            
+            # block 3
+            tmp = list(tmp_model[8].children())
+            self.rgb_skip_block_3 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.rgb_conv_block_3 = copy.deepcopy(tmp[2])
 
-                # block 1
-                tmp = list(tmp_model[6].children())
-                self.rgb_skip_block_1 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.rgb_conv_block_1 = copy.deepcopy(tmp[2])
+            self.depth_skip_block_3 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.depth_conv_block_3 = copy.deepcopy(tmp[2])
 
-                self.depth_skip_block_1 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.depth_conv_block_1 = copy.deepcopy(tmp[2])
+            self.concat_block_3 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.concat_block_3[0] = self._fix_concat_layer(self.concat_block_3)
 
-                self.concat_block_1 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.concat_block_1[0] = self._fix_concat_layer(self.concat_block_1)
-                                
-                # block 2
-                tmp = list(tmp_model[7].children())
-                self.rgb_skip_block_2 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.rgb_conv_block_2 = copy.deepcopy(tmp[2])
+            # block 4
+            self.rgb_conv_block_4 = copy.deepcopy(list(tmp_model[9].children())[0])
+            self.depth_conv_block_4 = copy.deepcopy(list(tmp_model[9].children())[0])
 
-                self.depth_skip_block_2 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.depth_conv_block_2 = copy.deepcopy(tmp[2])
+            tmp_conv = torch.nn.Conv2d(728, 728, kernel_size=3, stride=1, padding=1 bias=False)
+            tmp_batchnorm = torch.nn.BatchNorm2d(728, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+            torch.nn.init.xavier_uniform_(tmp_conv.weight)
 
-                self.concat_block_2 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.concat_block_2[0] = self._fix_concat_layer(self.concat_block_2)
+            self.concat_block_4 = torch.nn.Sequential(*([tmp_conv, tmp_batchnorm]))
 
-                
-                # block 3
-                tmp = list(tmp_model[8].children())
-                self.rgb_skip_block_3 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.rgb_conv_block_3 = copy.deepcopy(tmp[2])
+            # block 5
+            self.rgb_conv_block_5 = copy.deepcopy(list(tmp_model[10].children())[0])
+            self.depth_conv_block_5 = copy.deepcopy(list(tmp_model[10].children())[0])
 
-                self.depth_skip_block_3 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.depth_conv_block_3 = copy.deepcopy(tmp[2])
+            self.concat_block_5 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
 
-                self.concat_block_3 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.concat_block_3[0] = self._fix_concat_layer(self.concat_block_3)
+            # block 6
+            self.rgb_conv_block_6 = copy.deepcopy(list(tmp_model[11].children())[0])
+            self.depth_conv_block_6 = copy.deepcopy(list(tmp_model[11].children())[0])
 
-                # block 4
-                self.rgb_conv_block_4 = copy.deepcopy(list(tmp_model[9].children())[0])
-                self.depth_conv_block_4 = copy.deepcopy(list(tmp_model[9].children())[0])
+            self.concat_block_6 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
 
-                tmp_conv = torch.nn.Conv2d(728, 728, kernel_size=1, stride=1, bias=False)
-                tmp_batchnorm = torch.nn.BatchNorm2d(728, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-                torch.nn.init.xavier_uniform_(tmp_conv.weight)
+            # block 7
+            self.rgb_conv_block_7 = copy.deepcopy(list(tmp_model[12].children())[0])
+            self.depth_conv_block_7 = copy.deepcopy(list(tmp_model[12].children())[0])
 
-                self.concat_block_4 = torch.nn.Sequential(*([tmp_conv, tmp_batchnorm]))
+            self.concat_block_7 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
 
-                # block 5
-                self.rgb_conv_block_5 = copy.deepcopy(list(tmp_model[10].children())[0])
-                self.depth_conv_block_5 = copy.deepcopy(list(tmp_model[10].children())[0])
+            # block 8
+            self.rgb_conv_block_8 = copy.deepcopy(list(tmp_model[13].children())[0])
+            self.depth_conv_block_8 = copy.deepcopy(list(tmp_model[13].children())[0])
 
-                self.concat_block_5 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
+            self.concat_block_8 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
 
-                # block 6
-                self.rgb_conv_block_6 = copy.deepcopy(list(tmp_model[11].children())[0])
-                self.depth_conv_block_6 = copy.deepcopy(list(tmp_model[11].children())[0])
+            # block 9
+            self.rgb_conv_block_9 = copy.deepcopy(list(tmp_model[14].children())[0])
+            self.depth_conv_block_9 = copy.deepcopy(list(tmp_model[14].children())[0])
 
-                self.concat_block_6 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
+            self.concat_block_9 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
 
-                # block 7
-                self.rgb_conv_block_7 = copy.deepcopy(list(tmp_model[12].children())[0])
-                self.depth_conv_block_7 = copy.deepcopy(list(tmp_model[12].children())[0])
+            # block 10
+            self.rgb_conv_block_10 = copy.deepcopy(list(tmp_model[15].children())[0])
+            self.depth_conv_block_10 = copy.deepcopy(list(tmp_model[15].children())[0])
 
-                self.concat_block_7 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
+            self.concat_block_10 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
 
-                # block 8
-                self.rgb_conv_block_8 = copy.deepcopy(list(tmp_model[13].children())[0])
-                self.depth_conv_block_8 = copy.deepcopy(list(tmp_model[13].children())[0])
+            # block 11
+            self.rgb_conv_block_11 = copy.deepcopy(list(tmp_model[16].children())[0])
+            self.depth_conv_block_11 = copy.deepcopy(list(tmp_model[16].children())[0])
 
-                self.concat_block_8 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
+            self.concat_block_11 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
 
-                # block 9
-                self.rgb_conv_block_9 = copy.deepcopy(list(tmp_model[14].children())[0])
-                self.depth_conv_block_9 = copy.deepcopy(list(tmp_model[14].children())[0])
+            # block 12
+            tmp = list(tmp_model[17].children())
+            self.rgb_skip_block_12 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.rgb_conv_block_12 = copy.deepcopy(tmp[2])
 
-                self.concat_block_9 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
+            self.depth_skip_block_12 =copy.deepcopy( torch.nn.Sequential(*(tmp[:2])))
+            self.depth_conv_block_12 = copy.deepcopy(tmp[2])
 
-                # block 10
-                self.rgb_conv_block_10 = copy.deepcopy(list(tmp_model[15].children())[0])
-                self.depth_conv_block_10 = copy.deepcopy(list(tmp_model[15].children())[0])
+            self.concat_block_12 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
+            self.concat_block_12[0] = self._fix_concat_layer(self.concat_block_12)
+            
+            # net after block 12
+            self.rgb_model_final = copy.deepcopy(torch.nn.Sequential(*(tmp_model[18:25])))
+            self.depth_model_final = copy.deepcopy(torch.nn.Sequential(*(tmp_model[18:25])))
 
-                self.concat_block_10 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
+            self.fc_layer = torch.nn.Linear(in_features = 4096, out_features = 2, bias = True)
+            torch.nn.init.xavier_uniform_(self.fc_layer.weight)
 
-                # block 11
-                self.rgb_conv_block_11 = copy.deepcopy(list(tmp_model[16].children())[0])
-                self.depth_conv_block_11 = copy.deepcopy(list(tmp_model[16].children())[0])
-
-                self.concat_block_11 = copy.deepcopy(torch.nn.Sequential(*([tmp_conv, tmp_batchnorm])))
-
-                # block 12
-                tmp = list(tmp_model[17].children())
-                self.rgb_skip_block_12 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.rgb_conv_block_12 = copy.deepcopy(tmp[2])
-
-                self.depth_skip_block_12 =copy.deepcopy( torch.nn.Sequential(*(tmp[:2])))
-                self.depth_conv_block_12 = copy.deepcopy(tmp[2])
-
-                self.concat_block_12 = copy.deepcopy(torch.nn.Sequential(*(tmp[:2])))
-                self.concat_block_12[0] = self._fix_concat_layer(self.concat_block_12)
-                
-                # net after block 12
-                self.rgb_model_final = copy.deepcopy(torch.nn.Sequential(*(tmp_model[18:25])))
-                self.depth_model_final = copy.deepcopy(torch.nn.Sequential(*(tmp_model[18:25])))
-
-                self.fc_layer = torch.nn.Linear(in_features = 4096, out_features = 2, bias = True)
-                torch.nn.init.xavier_uniform_(self.fc_layer.weight)
-
-                self.model = None
-
-                # self.depth_model = DepthFake(conf)
-
-
-                # #remove rgb input and take only depth input in the first layer
-                # weights = self.depth_model.model.conv1.weight
-                # kernel_size = tuple(self.depth_model.model.conv1.kernel_size)
-                # stride = tuple(self.depth_model.model.conv1.stride)
-                # out_features = weights.shape[0]
-                # new_features = torch.nn.Conv2d(
-                #     in_features, out_features, kernel_size=kernel_size, stride=stride
-                # )
-                # new_features.weight = torch.nn.Parameter(weights.data[:, 3:, :, :])
-                # self.depth_model.model.conv1 = new_features
-
-                # # split = self.conf.run.double_depth_network.split
-
-                # # n = sum(1 for _ in self.rgb_model.model.parameters())
-                # # half = int(n/2)
-
-                # # for i, param in enumerate(self.rgb_model.model.parameters()):
-                # #     if i > half:
-                # #         break
-                # #     param.requires_grad = False
-                # # for i, param in enumerate(self.depth_model.model.parameters()):
-                # #     if i > half:
-                # #         break
-                # #     param.requires_grad = False
-                # self.rgb_model = torch.nn.Sequential(*(list(self.rgb_model.model.children())[:-1]))
-                # self.depth_model = torch.nn.Sequential(*(list(self.depth_model.model.children())[:-1]))
-                # # self.concat_layer = torch.nn.Conv2d(
-                # #     1456, 728, kernel_size=(1,1), stride=(1,1), padding = (0,0)
-                # # )
-
-                # self.theta = torch.nn.Parameter(torch.ones(2048)*0.5)
-                # self.ones = torch.ones(2048).to('cuda')
-
-                # self.fc_layer = torch.nn.Linear(in_features = 2048, out_features = 2, bias = True)
-                # # torch.nn.init.xavier_uniform_(self.concat_layer.weight)
-                # torch.nn.init.xavier_uniform_(self.fc_layer.weight)
-                # # self.model = torch.nn.Sequential(*(list(self.model.children())[split:])) 
-                # self.model = None
+            self.model = None
 
 
         else:
@@ -405,7 +313,7 @@ class DepthFakeRocket(pl.LightningModule):
         stride = tuple(layer[0].stride)
         out_features = weights.shape[0]
         new_features = torch.nn.Conv2d(
-            out_features, out_features, kernel_size=kernel_size, stride=1, bias=False
+            out_features, out_features, kernel_size=3, stride=1, padding=1 bias=False
         )
         torch.nn.init.xavier_uniform_(new_features.weight)
         return new_features
